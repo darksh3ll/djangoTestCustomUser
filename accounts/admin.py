9e767278a444d6f66685607169da380e52436207
darksh3ll <darksh3ll.js@gmail.com>
2023-02-05 19:14:11 +0100

    testing

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from .models import CustomUser
 
 admin.site.register(CustomUser)
